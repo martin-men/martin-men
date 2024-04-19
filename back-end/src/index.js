c6035d99cb3d2d85ba35f13d6c1a73a5de237fc1
Martin Mendieta <marmenthor@gmail.com>
2024-04-18 22:07:43 -0500

    Cors configuration

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,28 +1,34 @@
-import express from 'express'
-import cors from 'cors'
-import experienceRoutes from './routes/experience.routes.js'
-import projectsRoutes from './routes/projects.routes.js'
-import skillsRoutes from './routes/skills.routes.js'
-import allRoutes from './routes/all.routes.js'
+import express from "express";
+import cors from "cors";
+import experienceRoutes from "./routes/experience.routes.js";
+import projectsRoutes from "./routes/projects.routes.js";
+import skillsRoutes from "./routes/skills.routes.js";
+import allRoutes from "./routes/all.routes.js";
 
-const app = express()
+const app = express();
 let corsOptions = {
-  origin: ['http://localhost:5173', 'https://martin-men-y12i.vercel.app/']
+  origin: [
+    "http://localhost:5173",
+    "https://martin-men-y12i.vercel.app",
+    "https://martin-men-y12i-9k1m2a7yp-martin-mens-projects.vercel.app",
+  ],
 };
 
 // Middleware to parse JSON data
-app.use(express.json())
+app.use(express.json());
 app.use(cors(corsOptions));
 
 // Routes
-app.use('/martin-men', experienceRoutes)
-app.use('/martin-men', projectsRoutes)
-app.use('/martin-men', skillsRoutes)
-app.use('/martin-men', allRoutes)
-app.get('/', (req, res) => {res.send('Listening')})
+app.use("/martin-men", experienceRoutes);
+app.use("/martin-men", projectsRoutes);
+app.use("/martin-men", skillsRoutes);
+app.use("/martin-men", allRoutes);
+app.get("/", (req, res) => {
+  res.send("Listening");
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
+  console.log("Server is running on http://localhost:3000");
+});
 
-export default app
+export default app;
